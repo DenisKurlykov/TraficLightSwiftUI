4ce94850414ae8a466c0b18630cbb660b30f43ec
DenisKurlykov <dkurlykov@gmail.com>
2022-09-08 23:17:33 +0400

    Initial Commit

---
+++ TraficLightSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TraficLightSwiftUI
+//
+//  Created by Денис Курлыков on 08.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TraficLightSwiftUI/TraficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraficLightSwiftUIApp.swift
+//  TraficLightSwiftUI
+//
+//  Created by Денис Курлыков on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TraficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
